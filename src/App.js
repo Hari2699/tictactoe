c6debfb52e2c647b9425d21203279693e7a01513
Hariharan Baskar <hariharanbas@gmail.com>
2021-07-17 00:22:02 +0530

    Added more functions

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ const tiktacArray = new Array(9).fill("");
 const App = () => {
   let [isCross, setIsCross] = useState(true);
   let [winMessage, setWinMessage] = useState("");
+  let flag = false;
 
   const playAgain = () => {
     setIsCross(true);
     setWinMessage("");
     tiktacArray.fill("");
-    console.log("Hello");
   };
 
   const findWinner = () => {
@@ -26,50 +26,58 @@ const App = () => {
       tiktacArray[0] === tiktacArray[2] &&
       tiktacArray[0] !== ""
     ) {
-      setWinMessage(tiktacArray[0] + " Has Won !!! ");
+      setWinMessage(tiktacArray[0] + " Won the Game !!! ");
     } else if (
       tiktacArray[3] === tiktacArray[4] &&
       tiktacArray[3] === tiktacArray[5] &&
       tiktacArray[3] !== ""
     ) {
-      setWinMessage(tiktacArray[3] + " Has Won !!! ");
+      setWinMessage(tiktacArray[3] + " Won the Game !!! ");
     } else if (
       tiktacArray[6] === tiktacArray[7] &&
       tiktacArray[6] === tiktacArray[8] &&
       tiktacArray[6] !== ""
     ) {
-      setWinMessage(tiktacArray[6] + " Has Won !!! ");
+      setWinMessage(tiktacArray[6] + " Won the Game !!! ");
     } else if (
       tiktacArray[0] === tiktacArray[3] &&
       tiktacArray[0] === tiktacArray[6] &&
       tiktacArray[0] !== ""
     ) {
-      setWinMessage(tiktacArray[0] + " Has Won !!! ");
+      setWinMessage(tiktacArray[0] + " Won the Game !!! ");
     } else if (
       tiktacArray[1] === tiktacArray[4] &&
       tiktacArray[1] === tiktacArray[7] &&
       tiktacArray[1] !== ""
     ) {
-      setWinMessage(tiktacArray[1] + " Has Won !!! ");
+      setWinMessage(tiktacArray[1] + " Won the Game !!! ");
     } else if (
       tiktacArray[2] === tiktacArray[5] &&
       tiktacArray[2] === tiktacArray[8] &&
       tiktacArray[2] !== ""
     ) {
-      setWinMessage(tiktacArray[2] + " Has Won !!! ");
+      setWinMessage(tiktacArray[2] + " Won the Game !!! ");
     } else if (
       tiktacArray[2] === tiktacArray[4] &&
       tiktacArray[2] === tiktacArray[6] &&
       tiktacArray[2] !== ""
     ) {
-      setWinMessage(tiktacArray[2] + " Has Won !!! ");
+      setWinMessage(tiktacArray[2] + " Won the Game !!! ");
     } else if (
       tiktacArray[0] === tiktacArray[4] &&
       tiktacArray[0] === tiktacArray[8] &&
       tiktacArray[0] !== ""
     ) {
-      setWinMessage(tiktacArray[0] + " Has Won !!! ");
-    } 
+      setWinMessage(tiktacArray[0] + " Won the Game !!! ");
+    }
+    if (
+      tiktacArray.length === 9 &&
+      !tiktacArray.includes("") &&
+      flag === false
+    ) {
+      setWinMessage("Draw !!!");
+      return;
+    }
   };
 
   const changeItem = (index) => {
@@ -87,18 +95,18 @@ const App = () => {
     findWinner();
   };
 
-
   return (
     <Container className="p-5">
       <ToastContainer position="bottom-center"> </ToastContainer>
       <Row>
-        
         <Col md={6} className="offset-md-3">
-          
           {winMessage ? (
             <div className="header">
               <h1>{winMessage}</h1>
-              <Button onClick={playAgain} color="warning" > Play Again</Button>
+              <Button onClick={playAgain} color="warning">
+                {" "}
+                Play Again
+              </Button>
             </div>
           ) : (
             <h1>{isCross ? "Cross's Turn" : "Circle's Turn"}</h1>
